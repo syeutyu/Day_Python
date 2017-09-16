eb4f087d693f5704e86f4e7a76ae4a34751b5b54
syeutyu <syeuty@naver.com>
2017-09-17 01:10:39 +0900

    Python정리

--- 05_Tuple/Tuple.py
+++ 05_Tuple/Tuple.py
@@ -3,4 +3,4 @@ print(tuple) #('this','is','tuple')
 len(tuple) # 3
 print(tuple[0]) # this
 # tuple[1] = "Tuple is No change" 튜플은 리스트와는 다르게 안의 인덱스값을 변환할수는 없다.
-print(tuple[0:1]) # ('this',) 튜플, 리스트 slice 방법
+print(tuple[-1:1]) # ('this',) 튜플, 리스트 slice 방법
