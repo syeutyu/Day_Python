d4c48eadc40f2e31a539a31a4b8e2f5c2de821d5
syeutyu <syeuty@naver.com>
2017-10-22 00:00:32 +0900

    for문 공부중

---
+++ Project/main.py
@@ -0,0 +1,8 @@
+array = ["123","test","app"]
+for i in array :
+    print(i)
+str = '값'
+for i in range(1,10):
+    print("%d번째 출력" %i)
+    for j in range(1,10):
+        print(i, "X", j, "=", i * j)
